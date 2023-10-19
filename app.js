90505ccedae4e63af2755c317feb98fb8e80e889
Eugene <eugenehelland@mail.com>
2023-10-19 10:21:17 -0600

    Add the '+ Add Project' button DOM object, create file structure for js logic.

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 // DOM objects
 
-// Main display area for the content
-const contentArea = document.querySelector("#content-area");
-console.log("contentArea: ", contentArea);
-
-// Unordered list that displays the nav menu for tasks
+// Unordered list that displays the nav menu for tasks.
 const tasksNavMenu = document.querySelector("#tasks-nav");
 console.log("tasksNavMenu: ", tasksNavMenu);
 
 const taskButtons = tasksNavMenu.querySelectorAll(".nav-item");
 console.log("taskButtons: ", taskButtons);
 
-// Unordered list that displays the nav menus for user-registered projects
+// Unordered list that displays the nav menus for user-registered projects.
 const projectsNav = document.querySelector("#projects-nav");
 console.log("projectsNav: ", projectsNav);
 
 taskButtons.forEach(listItem => {
+  // Make each list item clickable.
   console.log("listItem: ", listItem);
   listItem.addEventListener("click", () =>
     alert(`clicked ${listItem.textContent}`)
   );
 });
+
+// Add new-project button.
+const addNewProjectBtn = document.querySelector("#add-project-div");
+console.log("addNewProjectBtn: ", addNewProjectBtn);
+
+addNewProjectBtn.addEventListener("click", () => {
+  alert("Clicked the add new project button.");
+});
+
+// Main display area for the content.
+const contentArea = document.querySelector("#content-area");
+console.log("contentArea: ", contentArea);
