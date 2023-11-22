961a9f518dc1eef36eae1ea7e83186cc3f0caf2e
Eugene <eugenehelland@mail.com>
2023-11-22 14:16:10 -0700

    Basic HTML for the mobile menu is added when hamburger icon is clicked.

--- scripts/helpers/mobile-menu.js
+++ scripts/helpers/mobile-menu.js
@@ -1,7 +1,24 @@
+function mobileMenuHTML() {
+  const mobileMenuHTML = `
+  <div id="mobile-menu">
+    <span id="mobile-dates-label">Dates:</span>
+    <ul id="mobile-menu-dates">
+    </ul>
+    <span id="mobile-projects-label">Projects:</span>
+    <ul id="mobile-menu-projects">
+    </ul>
+  </div>
+  `;
+
+  return mobileMenuHTML;
+}
+
 export function hamburgerFunctionality() {
   const hamburgerIcon = document.querySelector("#hamburger-icon");
+  const main = document.querySelector("main");
+  console.log("main: ", main);
 
   hamburgerIcon.addEventListener("click", () => {
-    alert("Hamburger functionality currently in development");
+    main.insertAdjacentHTML("afterbegin", mobileMenuHTML());
   });
 }
