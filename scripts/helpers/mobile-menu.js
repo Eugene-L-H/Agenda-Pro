1607fee971708efe9cc4ded22ebc6cc85c2b2985
Eugene <eugenehelland@mail.com>
2023-11-22 19:02:19 -0700

    Fix css errors. HTML for mobile menu appears in main area when mobile menu is clicked.

--- scripts/helpers/mobile-menu.js
+++ scripts/helpers/mobile-menu.js
@@ -2,8 +2,10 @@ function mobileMenuHTML() {
   const mobileMenuHTML = `
   <div id="mobile-menu">
     <span id="mobile-dates-label">Dates:</span>
-    <ul id="mobile-menu-dates">
-    </ul>
+    <span id="mobile-today">Today</span>
+    <span id="mobile-week">This Week</span>
+    <span id="mobile-month">This Month</span>
+    <span id="mobile-year">This Year</span>
     <span id="mobile-projects-label">Projects:</span>
     <ul id="mobile-menu-projects">
     </ul>
@@ -16,9 +18,12 @@ function mobileMenuHTML() {
 export function hamburgerFunctionality() {
   const hamburgerIcon = document.querySelector("#hamburger-icon");
   const main = document.querySelector("main");
-  console.log("main: ", main);
+
+  const taskList = document.querySelector("#mobile-menu-tasks");
+  const projectList = document.querySelector("#mobile-menu-projects");
 
   hamburgerIcon.addEventListener("click", () => {
+    hamburgerIcon.classList.toggle("menu-open");
     main.insertAdjacentHTML("afterbegin", mobileMenuHTML());
   });
 }
