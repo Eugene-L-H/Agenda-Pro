6d7594cbbddc9a2aee91ddb0457d478d50b3efa9
Eugene <eugenehelland@mail.com>
2023-11-27 16:46:02 -0700

    Can create sample arrays of tasks, and objects, to be used for demonstrating app functionality.

--- scripts/helpers/example-arrays.js
+++ scripts/helpers/example-arrays.js
@@ -1,3 +1,65 @@
+function populateExampleArray(type, numberOfObjects) {
+  const exampleArray = [];
+
+  // Create 16 tasks
+  for (let i = 0; i < numberOfObjects; i++) {
+    const object = type === "task" ? exampleTask(i) : exampleProject(i);
+
+    exampleArray.push(object);
+  }
+
+  return exampleArray;
+}
+
+function exampleTask(iteration) {
+  const randomChecked = Math.floor(Math.random() * 4);
+
+  let dateRange = returnDateRange(iteration);
+
+  let project = Math.floor(Math.random() * 5) + 1;
+
+  const task = {};
+  task.id = Date.now() + iteration;
+  task.checked = randomChecked === 3 ? true : false;
+  task.name = `Task ${iteration}`;
+  task.description = generateLoremIpsumSentence();
+  task.dueDate = randomDueDate(dateRange);
+  task.priority = Math.floor(Math.random() * 3) + 1;
+  task.project = `Project ${project}`;
+
+  return task;
+}
+
+function exampleProject(iteration) {
+  const project = {};
+  project.id = Date.now() + iteration;
+  project.name = `Task ${iteration}`;
+  project.description = generateLoremIpsumSentence();
+  project.priority = Math.floor(Math.random() * 3) + 1;
+  project.deadline = "None";
+
+  return project;
+}
+
+// Helper functions ---------------------------------------------- //
+
+function returnDateRange(iteration) {
+  let dateRange = "";
+
+  // Assign date range to task, based on iteration.
+  if (iteration < 3) {
+    dateRange = "today";
+  } else if (iteration < 8) {
+    dateRange = "week";
+  } else if (iteration < 12) {
+    dateRange = "month";
+  } else if (iteration < 16) {
+    dateRange = "year";
+  }
+
+  return dateRange;
+}
+
 function generateSpecificDate(daysFromToday) {
   const date = new Date(); // Today's date
   date.setDate(date.getDate() + daysFromToday); // Add 'daysFromToday' to the current date
@@ -35,72 +97,23 @@ function randomDueDate(dateRange) {
   return generateSpecificDate(random);
 }
 
-function populateExampleTasks(numberOfTasks) {
-  const exampleTasks = [];
-
-  // Create 16 tasks
-  for (let i = 0; i < numberOfTasks; i++) {
-    const task = exampleTask(i);
-
-    exampleTasks.push(task);
-  }
-
-  return exampleTasks;
-}
-
-function exampleTask(iteration) {
-  const randomChecked = Math.floor(Math.random() * 4);
-
-  let dateRange = returnDateRange(iteration);
-
-  let project = Math.floor(Math.random() * 5) + 1;
-
-  const task = {};
-  task.checked = randomChecked === 3 ? true : false;
-  task.description = generateLoremIpsumSentence();
-  task.dueDate = randomDueDate(dateRange);
-  task.priority = Math.floor(Math.random() * 3) + 1;
-  task.project = `Project ${project}`;
-
-  return task;
-}
-
-function exampleProject() {}
-
-function returnDateRange(iteration) {
-  let dateRange = "";
-
-  // Assign date range to task, based on iteration.
-  if (iteration < 3) {
-    dateRange = "today";
-  } else if (iteration < 8) {
-    dateRange = "week";
-  } else if (iteration < 12) {
-    dateRange = "month";
-  } else if (iteration < 16) {
-    dateRange = "year";
-  }
-
-  return dateRange;
-}
-
 // Lorem sentences for task descriptions
 function generateLoremIpsumSentence() {
   const loremIpsumSentences = [
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.",
+    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui of.",
     "Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.",
-    "Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris.",
+    "Nullam varius, turpis et commodo pharetra, est eros bibendum elit.",
     "Integer in mauris eu nibh euismod gravida.",
     "Duis ac tellus et risus vulputate vehicula.",
     "Donec lobortis risus a elit. Etiam tempor.",
-    "Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam.",
+    "Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis.",
     "Maecenas fermentum consequat mi. Donec fermentum.",
-    "Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque.",
-    "Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat.",
+    "Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec.",
+    "Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing.",
     "Cras mollis scelerisque nunc. Nullam arcu."
   ];
 
